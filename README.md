0f710b50490cee7b45fbc24bfb4bc705f39e07df
Dariusz Górak <darekg11@tlen.pl>
2021-11-21 20:15:23 +0100

    Add VueJS web framework. Add vuex to store global state. Add quasar UI framework. Server website from Express server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WIP
 
-Run like so `npm run dev -- --server_port=5000 --server_log_level=debug --mocha_tests_path='test-examples/**/*.spec.js'`
+Run like so:
+1. In first terminal `npm run watch-web-app` to build and hot-reload UI
+2. In second terminal `npm run dev -- --server_port=5000 --server_log_level=debug --mocha_tests_path='test-examples/**/*.spec.js'` to start server and UI on port `5000` with `DEBUG` log level and tests to run from relative `test-examples/**/*.spec.js` path
---
+++ lib/app/pages/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>Mocha UI Runner</q-toolbar-title>
+        <q-toggle @update:model-value="toggleDarkMode"
+                  v-model="useDarkMode"
+                  checked-icon="dark_mode"
+                  color="green"
+                  size="lg"
+                  unchecked-icon="light_mode"
+                  left-label
+                  label="Dark mode" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      width=500
+      bordered
+    >
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+export default {
+  setup() {
+    const $q = useQuasar();
+    const useDarkMode = ref(false);
+    return {
+      useDarkMode,
+      toggleDarkMode(value) {
+        $q.dark.set(value);
+      }
+    };
+  }
+}
+</script>
