155b400093bebbc04f5dfc59c1d526295c22d3fc
Dariusz Górak <darekg11@tlen.pl>
2021-11-24 20:59:49 +0100

    web - add dialog and toast Quasar services. Execute getInitialData action upon opening the runner UI

---
+++ lib/app/components/header/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-header elevated class="bg-primary text-white" height-hint="98">
+    <q-toolbar>
+      <q-toolbar-title>Mocha UI Runner</q-toolbar-title>
+      <q-toggle @update:model-value="toggleDarkMode"
+                  v-model="useDarkMode"
+                  checked-icon="dark_mode"
+                  color="green"
+                  size="lg"
+                  unchecked-icon="light_mode"
+                  left-label
+                  label="Dark mode" />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+export default {
+  setup() {
+    const $q = useQuasar();
+    const useDarkMode = ref(false);
+    return {
+      useDarkMode,
+      toggleDarkMode(value) {
+        $q.dark.set(value);
+      }
+    };
+  }
+}
+</script>
--- lib/app/pages/App.vue
+++ lib/app/pages/App.vue
@@ -1,22 +1,9 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title>Mocha UI Runner</q-toolbar-title>
-        <q-toggle @update:model-value="toggleDarkMode"
-                  v-model="useDarkMode"
-                  checked-icon="dark_mode"
-                  color="green"
-                  size="lg"
-                  unchecked-icon="light_mode"
-                  left-label
-                  label="Dark mode" />
-      </q-toolbar>
-    </q-header>
+    <app-header></app-header>
 
     <q-drawer
       show-if-above
-      v-model="leftDrawerOpen"
       side="left"
       width=500
       bordered
@@ -30,18 +17,9 @@
 </template>
 
 <script>
-import { ref } from "vue";
-import { useQuasar } from "quasar";
 export default {
-  setup() {
-    const $q = useQuasar();
-    const useDarkMode = ref(false);
-    return {
-      useDarkMode,
-      toggleDarkMode(value) {
-        $q.dark.set(value);
-      }
-    };
+  created() {
+    this.$store.dispatch("app/getInitialData");
   }
 }
 </script>
