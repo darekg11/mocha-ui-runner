7c38400ddf8300462edbf952c179fd6e1db599a5
Dariusz Górak <darekg11@tlen.pl>
2021-11-24 20:58:29 +0100

    cli - allow to modify Mocha's UI variable and Server's default interval in ms used by web app to check for run status

--- cli-args-parser.js
+++ cli-args-parser.js
@@ -11,12 +11,16 @@ class CommandLineParser {
     const parsed = yargs(hideBin(process.argv))
       .default("server_port", SERVER_DEFAULTS.PORT)
       .default("server_log_level", SERVER_DEFAULTS.LOG_LEVEL)
+      .default("server_progress_interval_ms", SERVER_DEFAULTS.PROGRESS_INTERVAL_MS)
       .default("mocha_tests_path", MOCHA_DEFAULTS.TESTS_PATH)
+      .default("mocha_ui", MOCHA_DEFAULTS.UI)
       .argv;
 
     return new Configuration({
       SERVER_PORT: parsed.server_port,
       SERVER_LOG_LEVEL: parsed.server_log_level,
+      SERVER_PROGRESS_INTERVAL_MS: parsed.server_progress_interval_ms,
+      MOCHA_UI: parsed.mocha_ui,
       MOCHA_TESTS_PATH: parsed.mocha_tests_path,
     });
   }
