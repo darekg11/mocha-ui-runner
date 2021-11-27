1dce1fc48ab14506cec9c487d3357ab1b932212c
Dariusz Górak <darekg11@tlen.pl>
2021-11-27 17:22:41 +0100

    Add stats to header component

--- lib/app/components/header/Header.vue
+++ lib/app/components/header/Header.vue
@@ -1,7 +1,21 @@
 <template>
-  <q-header elevated class="bg-primary text-white" height-hint="98">
+  <q-header elevated class="bg-primary text-white component-header-base" height-hint="98">
     <q-toolbar>
       <q-toolbar-title>Mocha UI Runner</q-toolbar-title>
+      <div class="status-panel">
+        <div class="single-info">
+          <p class="title">Status:</p><p class="value">{{ status }}</p>
+        </div>
+        <div class="single-info">
+          <p class="title">Tests glob:</p><p class="value">{{ testsPath }}</p>
+        </div>
+        <div class="single-info">
+          <p class="title">Total suites:</p><p class="value">{{ totalSuites }}</p>
+        </div>
+        <div class="single-info">
+          <p class="title">Total tests:</p><p class="value">{{ totalTests }}</p>
+        </div>
+      </div>
       <q-toggle @update:model-value="toggleDarkMode"
                   v-model="useDarkMode"
                   checked-icon="dark_mode"
@@ -11,13 +25,42 @@
                   left-label
                   label="Dark mode" />
     </q-toolbar>
+    <q-toolbar>
+      <div class="status-panel">
+        <div class="single-info">
+          <p class="title">Time:</p><p>-</p>
+        </div>
+        <div class="single-info">
+          <p class="title">Success tests:</p><p class="value success-tests-color">{{ successTests }}</p>
+        </div>
+        <div class="single-info">
+          <p class="title">Pending tests:</p><p class="value pending-tests-color">{{ pendingTests }}</p>
+        </div>
+        <div class="single-info">
+          <p class="title">Failing tests:</p><p class="value failed-tests-color">{{ failedTests }}</p>
+        </div>
+      </div>
+    </q-toolbar>
   </q-header>
 </template>
 
 <script>
 import { ref } from "vue";
+import { mapGetters } from 'vuex'
 import { useQuasar } from "quasar";
+import "./header.scss";
 export default {
+  computed: {
+    ...mapGetters("app", {
+      status: "status",
+      testsPath: "testsPath",
+      totalTests: "totalTests",
+      totalSuites: "totalSuites",
+      successTests: "successTests",
+      failedTests: "failedTests",
+      pendingTests: "pendingTests"
+    })
+  },
   setup() {
     const $q = useQuasar();
     const useDarkMode = ref(false);
